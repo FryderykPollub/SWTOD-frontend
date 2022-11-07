cef34e735b6c31fd0641756439d9040c3991b8bd
Fryderyk <fryderyk.malyszczuk@pollub.edu.pl>
2022-11-07 23:14:57 +0100

    Add validation to registration

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -15,7 +15,7 @@ import {
 import React, { useEffect, useState } from "react";
 import fetchApi from "../service/FetchService";
 import { useLocalStorage } from "../util/useLocalStorage";
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import UpperBar from "../naviBar/UpperBar";
 import { BASE_URL } from "../util/globalVars";
 
@@ -26,8 +26,19 @@ const Register = () => {
   const [lastName, setLastName] = useState("");
   const [dob, setDob] = useState("");
   const [title, setTitle] = useState("");
-  const [position, setPosition] = useState("");
+  const [position, setPosition] = useState(1);
   const [open, setOpen] = useState(false);
+  const [validationFlag, setValidationFlag] = useState(false);
+  const [emailHelperText, setEmailHelperText] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [nameHelperText, setNameHelperText] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [surnameHelperText, setSurnameHelperText] = useState("");
+  const [surnameError, setSurnameError] = useState(false);
+  const [dobHelperText, setDobHelperText] = useState("");
+  const [dobError, setDobError] = useState(false);
+  const [titleHelperText, setTitleHelperText] = useState("");
+  const [titleError, setTitleError] = useState(false);
 
   function sendRegisterRequest() {
     const reqBody = {
@@ -36,8 +47,8 @@ const Register = () => {
       surname: lastName,
       dob: dob,
       title: title,
-      positionId: position
-    }
+      positionId: position,
+    };
 
     let statusResponse;
 
@@ -53,8 +64,112 @@ const Register = () => {
           setOpen(true);
         }
       });
-
   }
+
+  function checkEmail(val) {
+    if (!val) {
+      setEmailHelperText("Email nie może być pusty");
+      setEmailError(true);
+    } else {
+      let regEmail =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!regEmail.test(val)) {
+        setEmailHelperText("Niepoprawny email");
+        setEmailError(true);
+      } else {
+        setEmailHelperText("");
+        setEmailError(false);
+        setEmail(val);
+      }
+    }
+  }
+
+  function checkName(val) {
+    if (!val) {
+      setNameHelperText("Imię nie może być puste");
+      setNameError(true);
+    } else {
+      let regName = /^[a-zA-ZąęółśżźćńĄĘÓŁŚŻŹĆŃ ,.'-]+$/;
+      if (!regName.test(val)) {
+        setNameHelperText("Imię zawiera błąd");
+        setNameError(true);
+      } else {
+        setNameHelperText("");
+        setNameError(false);
+        setFirstName(val);
+      }
+    }
+  }
+
+  function checkSurname(val) {
+    if (!val) {
+      setSurnameHelperText("Nazwisko nie może być puste");
+      setSurnameError(true);
+    } else {
+      let regSurname = /^[a-zA-ZąęółśżźćńĄĘÓŁŚŻŹĆŃ ,.'-]+$/;
+      if (!regSurname.test(val)) {
+        setSurnameHelperText("Nazwisko zawiera błąd");
+        setSurnameError(true);
+      } else {
+        setSurnameHelperText("");
+        setSurnameError(false);
+        setLastName(val);
+      }
+    }
+  }
+
+  function checkDob(val) {
+    if (!val) {
+      setDobHelperText("");
+      setDobError(false);
+      setDob(val);
+    } else {
+      let regDob = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+      if (!regDob.test(val)) {
+        setDobHelperText("Data urodzenia zawiera błąd");
+        setDobError(true);
+      } else {
+        setDobHelperText("");
+        setDobError(false);
+        setDob(val);
+      }
+    }
+  }
+
+  function checkTitle(val) {
+    if (!val) {
+      setTitleHelperText("Tytuł nie może być pusty");
+      setTitleError(true);
+    } else {
+      let regTitle = /^[a-zA-ZąęółśżźćńĄĘÓŁŚŻŹĆŃ ]+$/;
+      if (!regTitle.test(val)) {
+        setTitleHelperText("Tytuł zawiera błąd");
+        setTitleError(true);
+      } else {
+        setTitleHelperText("");
+        setTitleError(false);
+        setTitle(val);
+      }
+    }
+  }
+
+  function checkValidation() {
+    if (
+      !emailError &&
+      !nameError &&
+      !surnameError &&
+      !dobError &&
+      !titleError
+    ) {
+      setValidationFlag(true);
+    } else {
+      setValidationFlag(false);
+    }
+  }
+
+  useEffect(() => {
+    checkValidation();
+  }, [emailError, nameError, surnameError, dobError, titleError]);
 
   return (
     <>
@@ -84,7 +199,9 @@ const Register = () => {
             type="email"
             placeholder="example@mail.com"
             autoFocus
-            onChange={(e) => setEmail(e.target.value)}
+            error={emailError}
+            helperText={emailHelperText}
+            onChange={(e) => checkEmail(e.target.value)}
           />
           <TextField
             margin="normal"
@@ -94,7 +211,9 @@ const Register = () => {
             label="Imię"
             name="firstName"
             placeholder="Jan"
-            onChange={(e) => setFirstName(e.target.value)}
+            error={nameError}
+            helperText={nameHelperText}
+            onChange={(e) => checkName(e.target.value)}
           />
           <TextField
             margin="normal"
@@ -104,7 +223,9 @@ const Register = () => {
             label="Nazwisko"
             id="lastName"
             placeholder="Kowalski"
-            onChange={(e) => setLastName(e.target.value)}
+            error={surnameError}
+            helperText={surnameHelperText}
+            onChange={(e) => checkSurname(e.target.value)}
           />
           <TextField
             margin="normal"
@@ -113,7 +234,9 @@ const Register = () => {
             label="Data urodzenia"
             id="dob"
             placeholder="1970-01-31"
-            onChange={(e) => setDob(e.target.value)}
+            error={dobError}
+            helperText={dobHelperText}
+            onChange={(e) => checkDob(e.target.value)}
           />
           <TextField
             margin="normal"
@@ -123,7 +246,9 @@ const Register = () => {
             label="Tytuł"
             id="title"
             placeholder="Doktor"
-            onChange={(e) => setTitle(e.target.value)}
+            error={titleError}
+            helperText={titleHelperText}
+            onChange={(e) => checkTitle(e.target.value)}
           />
           <FormControl required fullWidth margin="normal">
             <InputLabel>Stanowisko</InputLabel>
@@ -131,7 +256,6 @@ const Register = () => {
               value={position}
               label="Stanowisko"
               onChange={(e) => setPosition(e.target.value)}
-              //placeholder={position}
             >
               <MenuItem value={1}>Profesor</MenuItem>
               <MenuItem value={2}>Profesor Uczelni</MenuItem>
@@ -143,6 +267,7 @@ const Register = () => {
             fullWidth
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
+            disabled={!validationFlag}
             onClick={() => sendRegisterRequest()}
           >
             Dodaj
